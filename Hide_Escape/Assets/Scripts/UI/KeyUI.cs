446ebedbb797b7c895660b813b35dd6928b08754
shusiro <shusiroseed@gmail.com>
2017-01-24 16:46:57 +0900

    インセクトゾンビ追加

--- Hide_Escape/Assets/Scripts/UI/KeyUI.cs
+++ Hide_Escape/Assets/Scripts/UI/KeyUI.cs
@@ -23,7 +23,7 @@ public class KeyUI : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         keyInfo = player.GetComponent<DoorManager>().Key.Count;
-
+        
         //  UIを鍵の数だけ生成
         keyUI = new Image[player.GetComponent<DoorManager>().Key.Count];
         activeKeyFlg = new bool[player.GetComponent<DoorManager>().Key.Count];
@@ -31,11 +31,18 @@ public class KeyUI : MonoBehaviour {
         //  鍵の数だけUIの情報を設定
         for(int i = 0; i < player.GetComponent<DoorManager>().Key.Count; ++i)
         {
-            keyUI[i] = (Image)Instantiate(key, new Vector3(80.0f + (i * 30.0f), 380.0f, 1.0f), new Quaternion(0.0f, 0.0f, 0.0f, 1.0f));
+            keyUI[i] = (Image)Instantiate(key);
+            keyUI[i].rectTransform.sizeDelta = new Vector2(key.rectTransform.sizeDelta.x * (Screen.width / 800), key.rectTransform.sizeDelta.y * (Screen.height / 400));
+            keyUI[i].rectTransform.position = new Vector3(Screen.width / 10 + ((i * 12) * keyUI[i].rectTransform.sizeDelta.x), Screen.height - (Screen.height / 10), 1.0f);
+            //keyUI[i].rectTransform.position = new Vector3(80, Screen.height - 40.0f, 1);
             //  keyUIを子に設定する
             keyUI[i].transform.parent = transform;
             activeKeyFlg[i] = false;
-            
+
+
+            activeKeyFlg[i] = true;
+            //  鍵のUIがアクティブ状態ならUIをアクティブにする
+            keyUI[i].gameObject.SetActive(activeKeyFlg[i]);
         }
 
 	}
@@ -49,11 +56,11 @@ public class KeyUI : MonoBehaviour {
             //  ドアが開かれたら鍵のUIを非アクティブ状態にする
             if (player.GetComponent<DoorManager>().Door[i].collider.isTrigger)
             {
-                activeKeyFlg[i] = false;
+                //activeKeyFlg[i] = false;
             }
 
             //  鍵のUIがアクティブ状態ならUIをアクティブにする
-            keyUI[i].gameObject.SetActive(activeKeyFlg[i]);
+            //keyUI[i].gameObject.SetActive(activeKeyFlg[i]);
 
             //  鍵がアクティブ状態なら飛ばす
             if (player.GetComponent<DoorManager>().Key[i].obj.activeInHierarchy) { continue; }
@@ -62,7 +69,7 @@ public class KeyUI : MonoBehaviour {
             //  鍵のUIがアクティブ状態じゃないならば鍵を表示してフラグをtrueにする
             if (activeKeyFlg[i] == false)
             {
-                activeKeyFlg[i] = true;
+                //activeKeyFlg[i] = true;
             }
 
 
